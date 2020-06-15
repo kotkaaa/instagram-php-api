8cbf60eed9a342a7298fc0b0b029f264c9791226
oleksandr <abamka@hotmail.com>
2020-06-16 00:00:49 +0300

    config setup

--- example/popular.php
+++ example/popular.php
@@ -7,7 +7,7 @@ use MetzWeb\Instagram\Instagram;
 
 $instagram = new Instagram($config['apiKey']);
 
-$result = $instagram->getPopularMedia();
+$result = $instagram->getMediaComments('CBV_B9kHhvX');
 
 ?>
 <!DOCTYPE html>
@@ -24,68 +24,13 @@ $result = $instagram->getPopularMedia();
 <div class="container">
     <header class="clearfix">
         <img src="assets/instagram.png" alt="Instagram logo">
-
         <h1>Instagram <span>popular photos</span></h1>
     </header>
     <div class="main">
-        <ul class="grid">
-            <?php
-            foreach ($result->data as $media) {
-                $content = '<li>';
-                // output media
-                if ($media->type === 'video') {
-                    // video
-                    $poster = $media->images->low_resolution->url;
-                    $source = $media->videos->standard_resolution->url;
-                    $content .= "<video class=\"media video-js vjs-default-skin\" width=\"250\" height=\"250\" poster=\"{$poster}\"
-                           data-setup='{\"controls\":true, \"preload\": \"auto\"}'>
-                             <source src=\"{$source}\" type=\"video/mp4\" />
-                           </video>";
-                } else {
-                    // image
-                    $image = $media->images->low_resolution->url;
-                    $content .= "<img class=\"media\" src=\"{$image}\"/>";
-                }
-                // create meta section
-                $avatar = $media->user->profile_picture;
-                $username = $media->user->username;
-                $comment = (!empty($media->caption->text)) ? $media->caption->text : '';
-                $content .= "<div class=\"content\">
-                           <div class=\"avatar\" style=\"background-image: url({$avatar})\"></div>
-                           <p>{$username}</p>
-                           <div class=\"comment\">{$comment}</div>
-                         </div>";
-                // output media
-                echo $content . '</li>';
-            }
-            ?>
-        </ul>
-        <!-- GitHub project -->
-        <footer>
-            <p>created by <a href="https://github.com/cosenary/Instagram-PHP-API">cosenary's Instagram class</a>,
-                available on GitHub</p>
-            <iframe width="95px" scrolling="0" height="20px" frameborder="0" allowtransparency="true"
-                    src="http://ghbtns.com/github-btn.html?user=cosenary&repo=Instagram-PHP-API&type=fork&count=true"></iframe>
-        </footer>
+        <pre>
+            <?php print_r($result) ?>
+        </pre>
     </div>
 </div>
-<!-- javascript -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-<script>
-    $(document).ready(function () {
-        // rollover effect
-        $('li').hover(
-            function () {
-                var $image = $(this).find('.media');
-                var height = $image.height();
-                $image.stop().animate({marginTop: -(height - 82)}, 1000);
-            }, function () {
-                var $image = $(this).find('.media');
-                var height = $image.height();
-                $image.stop().animate({marginTop: '0px'}, 1000);
-            }
-        );
-    });
-</script>
 </body>
 </html>
